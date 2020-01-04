25254f063d9a81e374cd4ce3e102f12f0efa2739
codecallogic <jfguardiacpw@gmail.com>
2020-01-04 13:13:40 -0800

    Added new folder coding challenges

---
+++ 1- Coding Challenges/averageScore.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+    let john = [89,120,103];
+    let mike = [116,94,123]; 
+    let mary = [97,134,105];
+
+    const arrSum = john.reduce((a,b) => a + b, 0);
+    const arrSum2 = mike.reduce((a,b) => a + b, 0);
+
+    if(arrSum > arrSum2 ){
+
+        console.log("John has the highest average score with " + arrSum);
+        
+    }else if(arrSum < arrSum2){
+
+        console.log("Mike has the highest avarage score with " + arrSum2);
+        
+    }else{
+        console.log("Its a draw both teams have the same average scores with John at " + arrSum + " and Mike at " + arrSum2);
+    }
+    
+    </script>
+</body>
+</html>
